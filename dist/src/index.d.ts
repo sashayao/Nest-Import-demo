bf8799274d726ccd82a28500c048e52daf50fa56
sasha yao <sasha.yao@move.com>
2021-02-28 20:07:48 -0800

    exporting 2

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,2 @@
 export { ExportModule } from './export.module';
 export { GreetService } from "./greet.service";
-export { MessageService } from "./message.service";
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,10 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MessageService = exports.GreetService = exports.ExportModule = void 0;
+exports.GreetService = exports.ExportModule = void 0;
 var export_module_1 = require("./export.module");
 Object.defineProperty(exports, "ExportModule", { enumerable: true, get: function () { return export_module_1.ExportModule; } });
 var greet_service_1 = require("./greet.service");
 Object.defineProperty(exports, "GreetService", { enumerable: true, get: function () { return greet_service_1.GreetService; } });
-var message_service_1 = require("./message.service");
-Object.defineProperty(exports, "MessageService", { enumerable: true, get: function () { return message_service_1.MessageService; } });
 //# sourceMappingURL=index.js.map
